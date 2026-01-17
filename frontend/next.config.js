5ea176a8a3e61517746b444a6a31d8b19b7826db
Fatim1509 <fatimshahbaz1509@gmail.com>
2026-01-18 02:56:13 +0500

    Update API URL to use HTTPS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'thursdaysnacks-production.up.railway.app',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://thursdaysnacks-production.up.railway.app',
   },
 }
 
